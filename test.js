c92163bba37bc449ddd2eae2b004b624c851cc27
Someant <594272732@qq.com>
2016-06-13 18:56:36 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,16 +2,36 @@ var fs = require("fs");
 var cheerio=require("cheerio");
 
 // 异步读取
-fs.readFile('test.html', function (err, data) {
+fs.readFile('shop.html', function (err, data) {
     if (err) {
         return console.error(err);
     }
 
+    var str=data.toString();
+
     var $=cheerio.load(str);
+
+    //images url
     $('img').each(function(){
-        var imgurl=$(this).attr('src');
-        console.log(imgurl);
+        var imgUrl=$(this).attr('src');
+        console.log(imgUrl);
     });
 
+    //css url
+    $('link').each(function(){
+        var cssUrl=$(this).attr('href');
+        console.log(cssUrl);
+    });
+
+    //script url
+    $('script').each(function(){
+        var jsUrl=$(this).attr('src');
+
+        if(jsUrl)
+        {
+            console.log(jsUrl);
+        }
+
+    });
 
 });
